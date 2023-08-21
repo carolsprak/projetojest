c01f3e599660da88fed5e3f98346cfe037b76c2f
Anne Caroline Rocha <carolsprak@gmail.com>
2023-08-21 20:55:20 -0300

    primeiros testes

---
+++ test/calculadora.test.js
@@ -0,0 +1,12 @@
+const {sum, mult, sub, div} = require('../src/calculadora.js');
+
+test('Verificar numeros positivos', ()=>{
+    expect(sum(2,7)).toBe(9);
+    expect(mult(5,3)).toBe(15);
+    expect(sub(2,7)).toBe(-5);
+    expect(div(35,3)).toBe(11.666666666666666);
+});
+
+test('Verificar numeros negativos', ()=>{
+    expect(sum(-2,2)).toBe(0);
+});
