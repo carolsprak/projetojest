da2a47cf03c22e01faeab3e8368d50b32d0b5936
Anne Caroline Rocha <carolsprak@gmail.com>
2024-02-27 21:09:02 -0300

    Update calculadora.test.js

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -2,6 +2,7 @@ const {sum, mult, sub, div} = require('../src/calculadora.js');
 
 test('Verificar numeros positivos', ()=>{
     expect(sum(2,7)).toBe(9); 
+    expect(sum(2,7)).toBe(7); 
     expect(mult(5,3)).toBe(15);
     expect(sub(2,7)).toBe(-5);
     expect(div(35,3)).toBe(11.666666666666666);
